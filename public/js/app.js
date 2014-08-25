15612cbb8c51cf578a53207a11442c35de8fe396
Ritesh Kumar <rkritesh078@gmail.com>
2014-08-26 00:35:01 +0530

    vendors added

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+/**
+* btpApp Module
+*
+* Crowd data analysis using Kalman Filters
+*/
+var app=angular.module('btpApp', ['ui.bootstrap']);
+
+app.controller('btpCtrl', ['$scope','$http', function($scope,$http){
+	
+	
+}]);
+
