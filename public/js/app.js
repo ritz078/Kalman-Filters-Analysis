1dc22dedf599a7222825ecae998353a92e0289d8
Ritesh Kumar <rkritesh078@gmail.com>
2014-08-31 03:33:26 +0530

    realtime json file creation and updation added

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,10 @@
 var app=angular.module('btpApp', ['ui.bootstrap']);
 
 app.controller('btpCtrl', ['$scope','$http', function($scope,$http){
-	
+	$http.get('/js/c.json').success(function(data){
+		$scope.x=data.cdata._ArrayData_.length;
+		console.log(data);
+	});
 	
 }]);
 
